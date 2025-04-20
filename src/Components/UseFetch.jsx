95c45deb1e7735377c8204440fade62c6948fa6b
Maiko Blaukopf <maiko-blaukopf@hotmail.com>
2025-04-20 18:11:28 -0400

    added custom hooks to fetch data and display them to front-end

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,12 @@
-const UseFetch = (url) => {
-    
+import React, { useEffect, useState } from 'react';
+
+ const useFetch = (url) => {
+    const[data,setData]=useState();
+    useEffect(()=>{ //This is a React Hook used for performing side effects in functional components. It’s typically used for fetching data, subscribing to events, or any other side effects that don’t involve rendering.
+        fetch(url).then((res)=>res.json()) //This initiates an HTTP request to the specified url.
+        .then((data)=>setData(data))
+    },[])
+  return [data]
 }
 
-export default UseFetch
+export default useFetch
